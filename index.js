eabff878743474a2999624982aca12fe8e4a12b4
Valssu <90453934+Valssu@users.noreply.github.com>
2021-10-08 14:18:59 +0300

    siirrä numerolista eri kohtaan

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+function arvoNumero(alaraja, ylaraja) {
+    return Math.floor(ylaraja * Math.random()) + alaraja
+}
+
 app.get('/', function (req, res) {
-    res.send('1 5 8 2 5')
+    let numerolista = [
+    arvoNumero(0,5),
+    arvoNumero(10,15),
+    arvoNumero(100,500),
+    arvoNumero(10,20),
+    arvoNumero(0,5),
+]
+    res.json(numerolista)
 })
     
 app.listen(PORT)
